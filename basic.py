242bea9fd25cf8222541207361a6fd7b660541c2
Cesar Miranda <cesarmirandacx602@gmail.com>
2024-08-05 22:27:06 -0700

    learned how to save and rename images after filter

--- basic.py
+++ basic.py
@@ -3,8 +3,16 @@ from PIL import Image
 #Create an image via import
 image = Image.open('gato.jpg')
 
-#analyze the image
-print(image.size)
+# #analyze the image
+# print(image.size)
+# print(image.filename)
+# print(image.format)
+
+# # flip/rotate image
+# image = image.transpose(Image.Transpose.ROTATE_90)
 
 # Show the image
 image.show()
+
+cat_rotated = image.rotate(30)
+cat_rotated.save('cat_rotated.png', 'png')
